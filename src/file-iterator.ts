842ddb899f2e5162e5731fc10a9c6e21d200a27f
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2025-02-07 11:15:11 -0600

    move listFilesSync to index.ts

--- src/file-iterator.ts
+++ src/file-iterator.ts
@@ -1,6 +1,7 @@
 import { program } from "commander";
 import fs from "fs";
 import path from "path";
+import { listFilesSync } from ".";
 
 /**
  * I used commander to simplify CLI argument handling and ensure the script is extensible for future features.
@@ -13,36 +14,6 @@ const [absolutePath] = program.parse().args;
 if (!absolutePath) {
   console.error("Error: No path provided. Please specify a directory.");
   process.exit(1);
-}
-
-function listFilesSync(dir: string, files: string[] = []): string[] {
-  try {
-    if (!fs.existsSync(dir)) {
-      console.error(`Error: Directory ${dir} does not exist.`);
-      process.exit(1);
-    }
-    const listing = fs.readdirSync(dir, { withFileTypes: true });
-    let dirs = [];
-    for (let f of listing) {
-      const fullName = path.join(dir, f.name);
-      if (f.isFile()) {
-        files.push(fullName);
-      } else if (f.isDirectory()) {
-        dirs.push(fullName);
-      }
-    }
-    for (let d of dirs) {
-      listFilesSync(d, files);
-    }
-
-    console.log("Done listing files");
-    return files;
-  } catch (e) {
-    console.error(
-      `Error: listing files in directory ${dir} failed with error ${e}. Skipping.`
-    );
-    return files;
-  }
 }
 
 console.log(`Listing files from: ${absolutePath}\n`);
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+export function listFilesSync(dir: string, files: string[] = []): string[] {
+  try {
+    if (!fs.existsSync(dir)) {
+      console.error(`Error: Directory ${dir} does not exist.`);
+      process.exit(1);
+    }
+    const listing = fs.readdirSync(dir, { withFileTypes: true });
+    let dirs = [];
+    for (let f of listing) {
+      const fullName = path.join(dir, f.name);
+      if (f.isFile()) {
+        files.push(fullName);
+      } else if (f.isDirectory()) {
+        dirs.push(fullName);
+      }
+    }
+    for (let d of dirs) {
+      listFilesSync(d, files);
+    }
+
+    console.log("Done listing files");
+    return files;
+  } catch (e) {
+    console.error(
+      `Error: listing files in directory ${dir} failed with error ${e}. Skipping.`
+    );
+    return files;
+  }
+}
