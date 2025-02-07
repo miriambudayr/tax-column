1b2b17c0beb0f35d3254fefcaf820ba2b07718f8
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2025-02-07 12:05:58 -0600

    add database setup test

--- src/inverted-index.test.ts
+++ src/inverted-index.test.ts
@@ -1,7 +1,17 @@
 import { InvertedIndex } from ".";
 import dotenv from "dotenv";
+import fs from "fs";
 import { assert } from "./assert";
 dotenv.config();
+
+describe("Database Setup", () => {
+  fit("should create the SQLite database file", () => {
+    new InvertedIndex();
+    const dbPath = process.env.DB_PATH;
+    assert(dbPath, "DB_PATH environment variable not found");
+    expect(fs.existsSync(dbPath)).toBe(true);
+  });
+});
 
 describe("Inverted Index", () => {
   it("should index a file", () => {
@@ -22,7 +32,7 @@ describe("Inverted Index", () => {
 
   it("should tokenize a directory", () => {
     const dirName = process.env.SAMPLE_DATA_DIR;
-    assert(dirName, "SAMPLE_DATA_DIR variable not found in environment");
+    assert(dirName, "SAMPLE_DATA_DIR environment variable not found");
     const invertedIndex = new InvertedIndex();
     invertedIndex.recursivelyIndexDirectory(dirName);
 
