abf3c4f2203b547e91100f21f05e64026594c83c
Miriam Budayr <35577323+miriambudayr@users.noreply.github.com>
2025-02-07 13:05:30 -0600

    reorder and rename describe blocks in test file

--- src/text-search-engine.test.ts
+++ src/text-search-engine.test.ts
@@ -6,110 +6,112 @@ dotenv.config();
 
 const TEST_DATA_DIR = "src/test_data";
 
-describe("Database Setup", () => {
-  beforeEach(() => {
-    if (fs.existsSync(DB_PATH)) {
-      fs.unlinkSync(DB_PATH);
-    }
-  });
-
-  afterEach(() => {
-    if (fs.existsSync(DB_PATH)) {
-      fs.unlinkSync(DB_PATH);
-    }
-  });
-
-  it("should create the SQLite database file", () => {
-    new TextSearchEngine();
-    const dbPath = process.env.DB_PATH;
-    assert(dbPath, "DB_PATH environment variable not found");
-    expect(fs.existsSync(dbPath)).toBe(true);
-  });
-});
-
-describe("Inverted Index", () => {
-  beforeEach(() => {
-    if (fs.existsSync(DB_PATH)) {
-      fs.unlinkSync(DB_PATH);
-    }
-  });
-
-  afterEach(() => {
-    if (fs.existsSync(DB_PATH)) {
-      fs.unlinkSync(DB_PATH);
-    }
-  });
-
-  it("should index and search a file", () => {
-    const fileName = `${TEST_DATA_DIR}/lorem-ipsum.txt`;
-    const invertedIndex = new TextSearchEngine();
-    invertedIndex.indexFile(fileName);
-
-    const files = invertedIndex.search("metus");
-
-    expect(files.length).toBeGreaterThan(0);
-
-    if (files) {
-      for (const file of files) {
-        expect(file).toEqual(fileName);
+describe("Text Search Engine", () => {
+  describe("Database Setup", () => {
+    beforeEach(() => {
+      if (fs.existsSync(DB_PATH)) {
+        fs.unlinkSync(DB_PATH);
       }
-    }
-  });
+    });
 
-  it("should search a phrase", () => {
-    const fileName = `${TEST_DATA_DIR}/lorem-ipsum.txt`;
-    const invertedIndex = new TextSearchEngine();
-    invertedIndex.indexFile(fileName);
-
-    const files = invertedIndex.searchPhrase("euismod ornare ultrices");
-
-    expect(files.length).toBeGreaterThan(0);
-
-    if (files) {
-      for (const file of files) {
-        expect(file).toEqual(fileName);
+    afterEach(() => {
+      if (fs.existsSync(DB_PATH)) {
+        fs.unlinkSync(DB_PATH);
       }
-    }
+    });
+
+    it("should create the SQLite database file", () => {
+      new TextSearchEngine();
+      const dbPath = process.env.DB_PATH;
+      assert(dbPath, "DB_PATH environment variable not found");
+      expect(fs.existsSync(dbPath)).toBe(true);
+    });
   });
 
-  it("should tokenize a directory", () => {
-    // TODO: try using actual data sample once this works. The dev feedback loop was way too slow
-    // to use the sample data for now.
-    // const dirName = process.env.SAMPLE_DATA_DIR;
-    // assert(dirName, "SAMPLE_DATA_DIR environment variable not found");
+  describe("File Indexing and Search", () => {
+    beforeEach(() => {
+      if (fs.existsSync(DB_PATH)) {
+        fs.unlinkSync(DB_PATH);
+      }
+    });
 
-    const dirName = TEST_DATA_DIR;
-    const invertedIndex = new TextSearchEngine();
-    invertedIndex.recursivelyIndexDirectory(dirName);
+    afterEach(() => {
+      if (fs.existsSync(DB_PATH)) {
+        fs.unlinkSync(DB_PATH);
+      }
+    });
 
-    let files = invertedIndex.search("porta");
-    expect(Array.from(files!)).toMatchSnapshot();
+    it("should index and search a file", () => {
+      const fileName = `${TEST_DATA_DIR}/lorem-ipsum.txt`;
+      const invertedIndex = new TextSearchEngine();
+      invertedIndex.indexFile(fileName);
 
-    files = invertedIndex.searchPhrase("porta magna urna et elit");
-    expect(Array.from(files!)).toMatchSnapshot();
-  });
+      const files = invertedIndex.search("metus");
 
-  it("should recursively tokenize a directory", () => {
-    const dirName = TEST_DATA_DIR;
-    const invertedIndex = new TextSearchEngine();
-    invertedIndex.recursivelyIndexDirectory(dirName);
+      expect(files.length).toBeGreaterThan(0);
 
-    let files = invertedIndex.search("simmons");
-    expect(Array.from(files!)).toMatchSnapshot();
+      if (files) {
+        for (const file of files) {
+          expect(file).toEqual(fileName);
+        }
+      }
+    });
 
-    files = invertedIndex.searchPhrase("dinner hosted by matt simmons");
-    expect(Array.from(files!)).toMatchSnapshot();
-  });
+    it("should search a phrase", () => {
+      const fileName = `${TEST_DATA_DIR}/lorem-ipsum.txt`;
+      const invertedIndex = new TextSearchEngine();
+      invertedIndex.indexFile(fileName);
 
-  it("should use case-insensitive search", () => {
-    const dirName = TEST_DATA_DIR;
-    const invertedIndex = new TextSearchEngine();
-    invertedIndex.recursivelyIndexDirectory(dirName);
+      const files = invertedIndex.searchPhrase("euismod ornare ultrices");
 
-    let files = invertedIndex.search("Simmons");
-    expect(Array.from(files!)).toMatchSnapshot();
+      expect(files.length).toBeGreaterThan(0);
 
-    files = invertedIndex.searchPhrase("Dinner hosted by Matt Simmons");
-    expect(Array.from(files!)).toMatchSnapshot();
+      if (files) {
+        for (const file of files) {
+          expect(file).toEqual(fileName);
+        }
+      }
+    });
+
+    it("should tokenize a directory", () => {
+      // TODO: try using actual data sample once this works. The dev feedback loop was way too slow
+      // to use the sample data for now.
+      // const dirName = process.env.SAMPLE_DATA_DIR;
+      // assert(dirName, "SAMPLE_DATA_DIR environment variable not found");
+
+      const dirName = TEST_DATA_DIR;
+      const invertedIndex = new TextSearchEngine();
+      invertedIndex.recursivelyIndexDirectory(dirName);
+
+      let files = invertedIndex.search("porta");
+      expect(Array.from(files!)).toMatchSnapshot();
+
+      files = invertedIndex.searchPhrase("porta magna urna et elit");
+      expect(Array.from(files!)).toMatchSnapshot();
+    });
+
+    it("should recursively tokenize a directory", () => {
+      const dirName = TEST_DATA_DIR;
+      const invertedIndex = new TextSearchEngine();
+      invertedIndex.recursivelyIndexDirectory(dirName);
+
+      let files = invertedIndex.search("simmons");
+      expect(Array.from(files!)).toMatchSnapshot();
+
+      files = invertedIndex.searchPhrase("dinner hosted by matt simmons");
+      expect(Array.from(files!)).toMatchSnapshot();
+    });
+
+    it("should use case-insensitive search", () => {
+      const dirName = TEST_DATA_DIR;
+      const invertedIndex = new TextSearchEngine();
+      invertedIndex.recursivelyIndexDirectory(dirName);
+
+      let files = invertedIndex.search("Simmons");
+      expect(Array.from(files!)).toMatchSnapshot();
+
+      files = invertedIndex.searchPhrase("Dinner hosted by Matt Simmons");
+      expect(Array.from(files!)).toMatchSnapshot();
+    });
   });
 });
